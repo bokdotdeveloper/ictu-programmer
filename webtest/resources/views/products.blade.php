02f1d2a3a6ee132f809a6ce873c5166e642a34ca
ysahannah <hleguro8@gmail.com>
2025-03-07 01:12:06 -0700

    updated products.blade.php

--- webtest/resources/views/products.blade.php
+++ webtest/resources/views/products.blade.php
@@ -84,6 +84,7 @@
                 class="w-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition">
                 Add Product
             </button>
+            <p>change 2</p>
         </form>
     </div>
 
